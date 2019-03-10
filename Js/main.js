98d22d224b355a77959a82d3b2fc4fb02a567bce
Pgarcia1989 <pjgl19@gmail.com>
2019-03-10 13:03:57 +0100

    añadido compilador Babel y archivo de configuracion

---
+++ Js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Nueva declaracion de variables
+var nombre = 'Carlos';
+console.log('Hola' + nombre);
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Nueva declaracion de variables
+
+let nombre = 'Carlos';
+
+console.log('Hola'+nombre);
+
